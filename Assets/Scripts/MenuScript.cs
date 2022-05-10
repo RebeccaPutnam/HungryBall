f4fc0a53f76885b0a776e8a04c8e4e1413d2b2e7
Jane Urciuoli <jurciuol@bowdoin.edu>
2022-05-10 14:36:41 +0200

    Main Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    //public Slider volumeSlider;
+    //public AudioMixer mixer;
+
+    public void SetVolume()
+    {
+        //mixer.SetFloat("volume", volumeSlider.value);
+    }
+
+    public void LoadLevel()
+    {
+        int random = Random.Range(2, 5);
+        SceneManager.LoadScene(random);
+    }
+
+    public void LoadTutorial(int integer)
+    {
+        SceneManager.LoadScene(integer);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
